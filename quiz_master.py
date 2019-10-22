caa1bd9cbcdc7235ee2c9ec8e85456d36919379d
ioanaMMT <ioana2morosanu@gmail.com>
2019-10-22 06:52:28 +0100

    update

--- quiz_master.py
+++ quiz_master.py
@@ -7,15 +7,19 @@ print("Nice to meet you {}!. Enjoy the quiz!".format(name))
 print("Let's go to the first question!")
 
 #.......QUESTION 1 .........
-question1 = int(raw_input("Question 1: Do you hane any cats? How many?"))
-if question1 == 0:
-	print("That's so sad! :( Cats are really fun loving creatures. You should get one.)")
-elif question1 <= 2:
-	print("Good for you {}. I bet you are having a great time together ;).".format(name))
-elif question1 <= 4:
-	print("OK...That must be a bit of work, but hey! I guess you are a cat lover :).")
-else:
-    print("OK...That is a bit creapy...You must REALLY love cats.")
+try:
+	question1 = int(raw_input("Question 1: Do you hane any cats? How many?"))
+	if question1 == 0:
+		print("That's so sad! :( Cats are really fun loving creatures. You should get one.)")
+	elif question1 <= 2:
+		print("Good for you {}. I bet you are having a great time together ;).".format(name))
+	elif question1 <= 4:
+		print("OK...That must be a bit of work, but hey! I guess you are a cat lover :).")
+	else:
+	    print("OK...That is a bit creapy...You must REALLY love cats.")
+except ValueError:
+	print("Please enter a integer number.")
+
 
 #.......QUESTION 2 ..........
 question2 = raw_input("Question 2: Are you having fun now? y/n")
@@ -23,6 +27,8 @@ if question2.lower() == "y":
 	print("Excellent! That means you like my quiz! Let's keep playing:)")
 elif question2.lower() == "n":
 	print("You are a tough cookie {}. You can exit the quiz by wrighting 'QUIT'.".format(name))	
+		
+
 
 #.......QESTION 3 ...........
 question3 = raw_input("Question 3: Did you know that your tongue is as long as your thumb? y/n")
@@ -53,6 +59,8 @@ elif question6 == 2:
 	print("The answer is: 'The baby of course. Because he is a little Bigger'.")
 elif question6 == 3:
 	print("Correct! Because he is a little Bigger.")
+else:
+	print("Please enter 1, 2 or 3.")
 
 #.......QUESTION 7 ..........
 question7 = int(raw_input("What is the answer to the next math problem  6/2(1+2) ?"))
