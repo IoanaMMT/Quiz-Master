d3b7a83a2e87010fa0bf775227d9a0289f611b0c
ioanaMMT <ioana2morosanu@gmail.com>
2019-11-06 17:27:30 +0000

    git sync

--- the_quiz.py
+++ the_quiz.py
@@ -10,21 +10,34 @@ questions = {
 }
 
 answers = {
-	1 : "Yes",
-	2 : "No",
-	3 : "Maybe",
-	4 : "Try again",
-	5 : "That's correct",
-	6 : "Very close",
-	7 : "Well done",
+	1 : ["Yes", "0", 1, 2, 3, range(4, 7)], # why numbers don't work? # how to create range of numbers
+	2 : ["No", "No2", "No3"],
+	3 : ["Maybe", "Maybe2", "Maybe3"],
+	4 : ["Try again", "Try again2", "Try again3"],
+	5 : ["That's correct", "That's correct2", "That's correct3"],
+	6 : ["Very close", "Very close2", "Very close3"],
+	7 : ["Well done", "Well done2", "Well done3"],
 	}
 
 #use slices for multiple answers and outcomes
+correct_answer = False
 
 for i in range(1, len(questions)+1):
-	answer = raw_input(questions[i])
+	answer = raw_input(questions[i]) # how to expect strings and integers 
 	
 	for i in range(1,len(answers)+1):
 		if answer == answers[i]:
 			print("T")
 
+		for x in answers[i]:
+			if answer == x:
+				correct_answer = True
+				print("Correct!")
+
+
+# Replace the test answers with the real ones
+# Have a different outcome to every answer
+# Capturing errors
+# Counting correct answers
+# Break the game when requested
+# List the results
